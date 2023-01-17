a9ca6bc508c727ae7c255626ec14d6eadef03e06
samfallahi <mfallahi@uncc.edu>
2023-01-17 15:26:57 -0500

    refactor: clean up main file

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from models import cgan
+from executor import executor_ann
+from data import data_reader, data_loader
 
-model = cgan.ANN()
-print(model)
+data_read = data_reader.DataReader()
+X, y = data_read.load_standardize_data('test')
+data_load = data_loader.DataModelLoader(X, y)
+train_loader, test_loader = data_load.train_test_data_loader()
+
+model_training = executor_ann.Training(train_loader, test_loader)
+
+model_training.exec()

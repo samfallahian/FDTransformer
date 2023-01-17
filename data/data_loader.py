600695452ce0ef54367a82acc31a73822338564d
samfallahi <mfallahi@uncc.edu>
2023-01-17 15:25:53 -0500

    refactor: add functions for standardize data

---
+++ data/data_loader.py
@@ -0,0 +1,27 @@
+import torch
+from sklearn.model_selection import train_test_split
+from utils import helpers
+from torch.utils.data import DataLoader
+
+
+class DataModelLoader:
+    def __init__(self, data, label):
+        """Convert to tensor"""
+        self.labels = torch.tensor(label).float()
+        self.data = torch.tensor(data).float()
+        """ Load  configurations """
+        config = helpers.Config()
+        cfg = config.from_json("data")
+        self.batch_size = cfg.batch_size
+
+    def train_test_data_loader(self):
+        """Split data into train and test"""
+        train_data, test_data, train_labels, test_labels = train_test_split(self.data, self.labels, test_size=.2)
+        """Convert into PyTorch Datasets"""
+        train_data_t = torch.utils.data.TensorDataset(train_data, train_labels)
+        test_data_t = torch.utils.data.TensorDataset(test_data, test_labels)
+        """Convert into dataloader objects"""
+        train_loader = DataLoader(train_data_t, batch_size=self.batch_size, shuffle=True)
+        test_loader = DataLoader(test_data_t, batch_size=test_data_t.tensors[0].shape[0])
+
+        return train_loader, test_loader
--- data/data_reader.py
+++ data/data_reader.py
@@ -1,16 +1,29 @@
 import pandas as pd
 from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
 from utils import helpers
 
 
 class DataReader:
     def __init__(self):
-        super.__init__()
+        # super.__init__()
         """ Load training configurations """
         config = helpers.Config()
         cfg = config.from_json("data")
-        df = pd.read_pickle(cfg.data_path + ".pkl", compression="zip")
+        self.path = cfg.data_path
 
-    def load_standardize_data(self):
-        pass
+    def load_standardize_data(self, file_name):
+        """ Read input file """
+        df = pd.read_pickle(self.path + file_name + ".pkl", compression="zip")
+        # labels = df.drop(df.columns.difference(["x", "y", "z", "time"]), axis=1).to_numpy()
+        labels = df.drop(df.columns.difference(["distance", "x"]), axis=1).to_numpy()
+        # data = df.drop(df.columns.difference(["vx", "vy", "vz", "px", "py", "pz", "distance"]), axis=1).to_numpy()
+        data = df.drop(df.columns.difference(["y", "z", "vx", "vy", "vz", "px", "py", "pz"]), axis=1).to_numpy()
+        """ Standardize data """
+        scalar = preprocessing.MinMaxScaler(feature_range=(-1, 1))
+        labels = scalar.fit_transform(labels)
+        data = scalar.fit_transform(data)
+        return data, labels
+    @staticmethod
+    def de_standardize(transformed_values):
+        scalar = preprocessing.MinMaxScaler(feature_range=(-1, 1))
+        return scalar.inverse_transform(transformed_values)

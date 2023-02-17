357188356b13f58ed9c69566cd1d0b33e952c283
samfallahian <mfallahi@uncc.edu>
2023-02-16 23:32:52 -0500

    refactor: change inputs

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     config = helpers.Config()
     cfg = config.from_json("data")
     data_read = data_reader.DataReader()
-    X, y = data_read.load_standardize_data('test_500')
+    X, y = data_read.load_standardize_data('wind_forecast')
     data_load = data_loader.DataModelLoader(X, y)
     train_loader = data_load.all_data_loader()
     model_training = executor_tgan.Training(train_loader)

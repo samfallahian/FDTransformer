b9f0c4e8396790e5088a5fce804dda60cd8f7c8d
samfallahian <mfallahi@uncc.edu>
2023-02-16 23:31:19 -0500

    refactor: add wind data reader

--- data/data_reader.py
+++ data/data_reader.py
@@ -11,17 +11,26 @@ class DataReader:
         cfg = config.from_json("data")
         self.path = cfg.data_path
 
-
     def load_standardize_data(self, file_name):
         """ Read input file """
-        df = pd.read_pickle(self.path + file_name + ".pkl", compression="zip")
-        labels = df.drop(df.columns.difference(["label"]), axis=1).to_numpy()
-        data = df.drop(["label"], axis=1).to_numpy()
+        ### PTV DATA
+        # df = pd.read_pickle(self.path + file_name + ".pkl", compression="zip")
+        # labels = df.drop(df.columns.difference(["label"]), axis=1).to_numpy()
+        # data = df.drop(["label"], axis=1).to_numpy()
+
+        ### Wind data
+        df = pd.read_csv(self.path + file_name + ".csv")
+        labels = df.drop(df.columns.difference(["time_frame", "farm"]), axis=1).to_numpy()
+        data = df.drop(["time_frame", "farm", "date"], axis=1).to_numpy()
+        print(labels.shape)
+        print(data.shape)
+
         """ Standardize data """
         scalar = preprocessing.MinMaxScaler(feature_range=(-1, 1))
         # labels = scalar.fit_transform(labels)
         data = scalar.fit_transform(data)
         return data, labels
+
     @staticmethod
     def de_standardize(transformed_values):
         scalar = preprocessing.MinMaxScaler(feature_range=(-1, 1))

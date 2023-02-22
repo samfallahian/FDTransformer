f9e8d9104664fb912244e629de85fe5b21ba6245
samfallahian <mfallahi@uncc.edu>
2023-02-21 22:55:51 -0500

    feat: add cae model

--- models/model_cae.py
+++ models/model_cae.py
@@ -13,46 +13,51 @@ class CAE(nn.Module):
         cfg_training = config.from_json("training")
         self.cfg = cfg
         self.cfg_training = cfg_training
-        """ Create dictionary to store the layers """
-        self.layers = nn.ModuleDict()
-        """ Define layers """
-        self.nLayers = len(cfg.caeUnits)
-        """Define input layer"""
-        self.layers["input_layer"] = nn.Linear(cfg.caeUnits[0], cfg.caeUnits[1])
-        """Define hidden layers and batch normalization"""
-        for i in range(1, len(cfg.caeUnits) - 2):
-            self.layers[f"hidden_{i}"] = nn.Linear(cfg.caeUnits[i], cfg.caeUnits[i + 1])
+
+        """ Encoder layers """
+        self.encoderLayers = nn.ModuleDict()
+        self.nEncoderLayers = len(cfg.encoderUnits)
+        self.encoderLayers["input_layer"] = nn.Linear(cfg.encoderUnits[0], cfg.encoderUnits[1])
+        # Hidden layers
+        for i in range(1, len(cfg.encoderUnits) - 2):
+            self.encoderLayers[f"hidden_{i}"] = nn.Linear(cfg.encoderUnits[i], cfg.encoderUnits[i + 1])
             # self.layers[f"batch_norm_{i}"] = nn.BatchNorm1d(cfg.discriminatorUnits[i])
-        """Define output layer"""
-        self.layers["output_layer"] = nn.Linear(cfg.caeUnits[-2], cfg.caeUnits[-1])
+        # output layer
+        self.encoderLayers["output_layer"] = nn.Linear(cfg.encoderUnits[-2], cfg.encoderUnits[-1])
 
-        self.encoder = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim),
-            nn.ReLU()
-        )
-        self.decoder = nn.Sequential(
-            nn.Linear(hidden_dim, input_dim),
-            nn.Sigmoid()
-        )
+        """ Decoder layers """
+        self.decoderLayers = nn.ModuleDict()
+        self.nDecoderLayers = len(cfg.decoderUnits)
+        self.decoderLayers["input_layer"] = nn.Linear(cfg.decoderUnits[0], cfg.decoderUnits[1])
+        # Hidden layers
+        for i in range(1, len(cfg.decoderUnits) - 2):
+            self.decoderLayers[f"hidden_{i}"] = nn.Linear(cfg.decoderUnits[i], cfg.decoderUnits[i + 1])
+            # self.layers[f"batch_norm_{i}"] = nn.BatchNorm1d(cfg.discriminatorUnits[i])
+        # output layer
+        self.decoderLayers["output_layer"] = nn.Linear(cfg.decoderUnits[-2], cfg.decoderUnits[-1])
 
-    def forward(self, x, label):
-        x = torch.cat((x, label), dim=1)
-        x = F.relu(self.layers["input_layer"](x))
-        x = F.dropout(x, p=self.cfg.dropout)
-        for i in range(1, self.nLayers - 2):
-            # x = self.layers[f"batch_norm_{i}"](x)
-            x = F.dropout(x, p=self.cfg.dropout)
-            x = F.relu(self.layers[f"hidden_{i}"](x))
-        x = self.layers[f"output_layer"](x)
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
         return x
 
     def encoder(self, x):
-        x = F.leaky_relu(self.layers["input_layer"](x), negative_slope=self.cfg.negative_slope)
+        x = F.relu(self.encoderLayers["input_layer"](x))
         x = F.dropout(x, p=self.cfg.dropout)
-        for i in range(1, self.nLayers - 2):
-            # x = self.layers[f"batch_norm_{i}"](x)
+        for i in range(1, self.nEncoderLayers - 2):
+            # x = self.encoderLayers[f"batch_norm_{i}"](x)
             x = F.dropout(x, p=self.cfg.dropout)
-            x = F.leaky_relu(self.layers[f"hidden_{i}"](x), negative_slope=self.cfg.negative_slope)
-        x = self.layers[f"output_layer"](x)
+            x = F.relu(self.encoderLayers[f"hidden_{i}"](x))
+        x = F.relu(self.encoderLayers["output_layer"](x))
+        return x
+
+    def decoder(self, x):
+        x = F.relu(self.decoderLayers["input_layer"](x))
+        x = F.dropout(x, p=self.cfg.dropout)
+        for i in range(1, self.nDecoderLayers - 2):
+            # x = self.decoderLayers[f"batch_norm_{i}"](x)
+            x = F.dropout(x, p=self.cfg.dropout)
+            x = F.relu(self.decoderLayers[f"hidden_{i}"](x))
+        x = torch.sigmoid(self.layers["output_layer"](x))
         return x
 

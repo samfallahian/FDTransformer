35ea01aac0bb4b983a237d7b01b5da60d99bd732
samfallahian <mfallahi@uncc.edu>
2023-03-05 17:00:57 -0500

    fix: contractive loss

--- models/loss.py
+++ models/loss.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
 from sklearn.neighbors import KernelDensity
 from scipy.stats import wasserstein_distance
 from utils import helpers
@@ -10,13 +11,14 @@ class CustomLoss(nn.Module):
     def __init__(self):
         super(CustomLoss, self).__init__()
         config = helpers.Config()
-        self.cfg = config.from_json("training")
+        self.cfg_cgan = config.from_json("training").cgan
+        self.cfg_cae = config.from_json("training").cae
         self.bec_log = nn.BCEWithLogitsLoss()
         self.bec = nn.BCELoss()
         self.mse = nn.MSELoss()
 
     def cgan_loss(self, generated_label, real_label, generated_data, real_data, is_generator=True):
-        if self.cfg.is_critic:
+        if self.cfg_cgan.is_critic:
             loss = self.bec_log(generated_label, real_label)
         else:
             loss = self.bec(generated_label, real_label)
@@ -28,24 +30,16 @@ class CustomLoss(nn.Module):
             final_loss = loss
         return final_loss
 
-    def cae_loss(self, encoded, decoded, data, encoder_model):
-        # mse_loss = self.mse(decoded,data)
-        # jacobian = torch.autograd.functional.jacobian(encoder_model, data)
-        # jacobian_norm = torch.norm(jacobian[0], dim=(0, 2))
-        # encoded_reshaped = encoded.view(-1, 1, encoded.shape[1])
-        # encoded_norm = torch.norm(encoded_reshaped, dim=(2))
-        # contractive_loss = torch.mean(jacobian_norm ** 2 * encoded_norm ** 2)
-        # final_loss = mse_loss + self.cfg.contractive_coef * contractive_loss
-
-        # Compute the regularization term based on the Jacobian matrix
-        jacobian = torch.autograd.functional.jacobian(encoder_model, data)
-        regularization = torch.mean(torch.norm(jacobian, dim=(2, 3)) ** 2)
-
-        # Add the regularization term to the loss function
-        final_loss =  self.mse(decoded,data) + self.cfg.contractive_coef *  regularization
-
+    def cae_loss(self, W, data, decoded, encoded):
+        mse = self.mse(decoded, data)
+        dh = encoded * (1 - encoded)  # Hadamard product produces size N_batch x N_hidden
+        # Sum through the input dimension to improve efficiency, as suggested in #1
+        w_sum = torch.sum(Variable(W) ** 2, dim=1)
+        # unsqueeze to avoid issues with torch.mv
+        w_sum = w_sum.unsqueeze(1)  # shape N_hidden x 1
+        contractive_loss = torch.sum(torch.mm(dh ** 2, w_sum), 0)
+        final_loss = mse + contractive_loss.mul(self.cfg_cae.contractive_coef)
         return final_loss
-
 
     def wass_distance(self, real, generated):
         """Use KernelDensity to estimate the probability density function (PDF) of the real and generated data. Then,
@@ -57,15 +51,13 @@ class CustomLoss(nn.Module):
         generated = generated.detach().numpy()
 
         # Estimate PDFs of real and generated data
-        kde_real = KernelDensity(kernel='gaussian', bandwidth=self.cfg.kd_band_width).fit(real)
-        kde_fake = KernelDensity(kernel='gaussian', bandwidth=self.cfg.kd_band_width).fit(generated)
+        kde_real = KernelDensity(kernel='gaussian', bandwidth=self.cfg_cgan.kd_band_width).fit(real)
+        kde_fake = KernelDensity(kernel='gaussian', bandwidth=self.cfg_cgan.kd_band_width).fit(generated)
         # Calculate Wasserstein distance between real and generated data
         real_pdf = kde_real.score_samples(real)
         fake_pdf = kde_fake.score_samples(generated)
         wass_distance = wasserstein_distance(real_pdf, fake_pdf)
         return wass_distance
-
-
 
     # @staticmethod
     # def kl_divergence(real, generated):

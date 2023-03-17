1cc1253192fbaa0bfa083578b45ba9141e406125
samfallahi <mfallahi@uncc.edu>
2023-03-16 21:41:02 -0400

    refactor: improve saving results

--- utils/helpers.py
+++ utils/helpers.py
@@ -29,11 +29,12 @@ class PythonObject(object):
 
 class Log:
 
-    def __init__(self, file_name):
+    def __init__(self, file_name="default"):
         self.file_name = file_name
 
     def write_file(self, data):
-        with open(os.path.join("logs", f"{datetime.now().strftime('%Y-%m-%d %H%M%S')}-{self.file_name}.json"), "w") as f:
+        with open(os.path.join("logs", f"{datetime.now().strftime('%Y-%m-%d %H%M%S')}-{self.file_name}.json"),
+                  "w") as f:
             json.dump(data, f, indent=4)
 
     def save_result(self, df):

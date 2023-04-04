0cfaf7e12155abc12e440db5fccf779b7f1c65ce
kkreth <kekreth@uncc.edu>
2023-04-04 14:51:25 -0400

    OK, so very close to the first training epoch actually working...big to-dos that are left are to SORT on x,y,z (euclidean) and maybe one other thing? I have to think on that. LOL

--- standalone/autoencoder.py
+++ standalone/autoencoder.py
@@ -1,9 +1,111 @@
 import pandas as pd
 import array
 import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import TensorDataset, DataLoader
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+#Put in the above, specifically to deal with :
+# FutureWarning: The input object of type 'Tensor' is an array-like implementing one of the corresponding protocols (`__array__`, `__array_interface__` or `__array_struct__`); but not a sequence (or 0-D). In the future, this object will be coerced as if it was first converted using `np.array(obj)`. To retain the old behaviour, you have to either modify the type 'Tensor', or assign to an empty array created with `np.empty(correct_shape, dtype=object)`.data = torch.from_numpy(np.asarray(data))
 
 #Read in the normalized df
-df = pd.read_pickle('/Users/kkreth/PycharmProjects/cgan/dataset/3p6_with_normalized_values_100000_sample.pkl.gz')
+#df = pd.read_pickle('/Users/kkreth/PycharmProjects/cgan/dataset/3p6_with_normalized_values_100000_sample.pkl.gz')
+df = pd.read_pickle('/Users/kkreth/PycharmProjects/cgan/dataset/3p6_with_normalized_values.pkl.gz')
+
+
+
+# Define the size of the latent representation
+latent_size = 10
+
+
+# Define the encoder architecture
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        self.conv1 = nn.Conv1d(3, 16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv1d(16, 32, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv1d(32, 64, kernel_size=3, padding=1)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(125 * 64, latent_size)
+
+    def forward(self, x):
+        x = nn.functional.relu(self.conv1(x))
+        x = nn.functional.relu(self.conv2(x))
+        x = nn.functional.relu(self.conv3(x))
+        x = self.flatten(x)
+        x = self.fc1(x)
+        return x
+
+
+# Define the decoder architecture
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.fc1 = nn.Linear(latent_size, 125 * 64)
+        self.unflatten = nn.Unflatten(1, (64, 125))
+        self.conv1 = nn.Conv1d(64, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv1d(32, 16, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv1d(16, 3, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.unflatten(x)
+        x = nn.functional.relu(self.conv1(x))
+        x = nn.functional.relu(self.conv2(x))
+        x = nn.functional.relu(self.conv3(x))
+        return x
+
+
+# Define the autoencoder architecture
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+
+# Define the training loop
+def train(model, data_loader, num_epochs, learning_rate, device):
+    # Set up the loss function and optimizer
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Move the model to the device
+    model = model.to(device)
+
+    # Train the model for the specified number of epochs
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        for data in data_loader:
+            # Move the data to the device
+            #Clearly pretty dangerous
+            #TODO Find out what a list was even here to begin with
+            data = data[0].to(device)
+            # Zero the parameter gradients
+            optimizer.zero_grad()
+            # Forward pass
+            outputs = model(data)
+            loss = criterion(outputs, data)
+            # Backward pass and optimization
+            loss.backward()
+            optimizer.step()
+            # Update the running loss
+            running_loss += loss.item()
+        # Print the epoch loss
+        epoch_loss = running_loss / len(data_loader)
+        print("Epoch {} loss: {:.4f}".format(epoch + 1, epoch_loss))
+        # Save the model every 100 epochs
+        if (epoch + 1) % 100 == 0:
+            model.save("model_epoch{}.pt".format(epoch + 1))
+
+# Define a sample input tensor
+
 
 
 #A helper function we will use later
@@ -125,27 +227,39 @@ def atomic_auto(df_reference):
     if (within_range_check(myx,myy,myz)):
         #First gather two lower (x for now)
         indices_x = np.where(x_array_coordinates == myx)[0]
-        #print(indices_x.dtype)
-        #x_one_below = x_array_coordinates[indices_x -1]
-        #x_two_below = x_array_coordinates[indices_x -2]
+        indices_y = np.where(y_array_coordinates == myy)[0]
+        indices_z = np.where(z_array_coordinates == myz)[0]
+        #Now to define the ranges we will tollerate/use
         lower_x = indices_x - 2
         upper_x = indices_x + 2
+        lower_y = indices_y - 2
+        upper_y = indices_y + 2
+        lower_z = indices_z - 2
+        upper_z = indices_z + 2
         #print(lower_x.dtype)
         #Now to get a scalar...because otherwise it doesn't work!
         lower_x = lower_x[0]
         upper_x = upper_x[0]
+        lower_y = lower_y[0]
+        upper_y = upper_y[0]
+        lower_z = lower_z[0]
+        upper_z = upper_z[0]
         '''
-        print(lower_x)
-        lower_x = 56
-        print(lower_x)
+        Now the actual 
         '''
         val_lx = x_array_coordinates[lower_x]
         val_ux = x_array_coordinates[upper_x]
+        val_ly = y_array_coordinates[lower_y]
+        val_uy = y_array_coordinates[upper_y]
+        val_lz = z_array_coordinates[lower_z]
+        val_uz = z_array_coordinates[upper_z]
         #print(valX)
         range_X = (val_lx, val_ux)
-        print(range_X)
+        range_Y = (val_ly, val_uy)
+        range_Z = (val_lz, val_uz)
+        #print(range_X)
         # Use boolean indexing to select rows with values within the specified ranges
-        mask = (df['x'].between(*range_X)) # & df['B'].between(*range_B) & df['C'].between(*range_C))
+        mask = (df['x'].between(*range_X) & df['y'].between(*range_Y) & df['z'].between(*range_Z) ) # & df['B'].between(*range_B) & df['C'].between(*range_C))
         #Apparently this has to be against the entire df or we have an indexing issue...
         result_partial = df[mask]
         #So now we take the time slice again...
@@ -161,6 +275,63 @@ def atomic_auto(df_reference):
 
 
 
+
+# Initialize counters for iterations with and without errors
+with_error = 0
+without_error = 0
+
+# Create a new instance of the autoencoder
+autoencoder = Autoencoder()
+
+# Set up the try block loop
+while True:
+    try:
+        # Get a random row from the DataFrame
+        # This shows as an interpreter error...but works. Bug?
+        df_unitTest = df.sample()
+        # Being arbritrary here and selecting only the first 'time' step
+        myTime = df_unitTest.time.iloc[0]
+        mask_time = (df['time'] == myTime)
+        relativeTimedf = df[mask_time]
+        result = atomic_auto(df_unitTest)
+
+        input_tensor = torch.tensor(result[['vx_norm','vy_norm','vz_norm']].values)
+        print("input tensor shape:")
+        print(input_tensor.shape)
+        #input_tensor = input_tensor.reshape(1, 375)
+        print("NEW input tensor shape:")
+        print(input_tensor.shape)
+        dataset = TensorDataset(input_tensor)
+
+        data_loader = DataLoader(dataset, batch_size=1)
+
+
+
+        # Train one epoch of the autoencoder
+        train(autoencoder, data_loader, num_epochs=1, learning_rate=0.001, device='mps')
+
+        print("result df length is: ")
+        print(len(result))
+        print(result)
+        # If the value is even, increment the counter for iterations without errors
+        without_error += 1
+    except ValueError as e:
+        # If a ValueError is raised, increment the counter for iterations with errors and print the error message
+        with_error += 1
+        print(e)
+    # Break out of the loop after a certain number of iterations
+    if with_error + without_error == 10:
+        break
+
+# Print the final counts of iterations with and without errors
+print("Iterations with error: {}".format(with_error))
+print("Iterations without error: {}".format(without_error))
+
+
+
+
+
+'''
 #This shows as an interpreter error...but works. Bug?
 df_unitTest = df.sample()
 #Being arbritrary here and selecting only the first 'time' step
@@ -168,6 +339,9 @@ myTime = df_unitTest.time.iloc[0]
 mask_time = (df['time'] == myTime)
 relativeTimedf = df[mask_time]
 result = atomic_auto(relativeTimedf)
-
+print("result df length is: ")
+print(len(result))
+print(result)
+'''
 
 

b9d965035949dfff9adca914ce47121c7430e4ca
Kyle Kreth <100863552+kekreth@users.noreply.github.com>
2023-06-01 16:28:38 -0400

    Works with arrays now as well, with a unit test to prove it...

--- standalone/TransformLatent.py
+++ standalone/TransformLatent.py
@@ -4,6 +4,7 @@ High of 2.6400
 Low of -1.9800
 We will use that here to transform on demand any nacent values
 '''
+import numpy as np
 
 class FloatConverter:
     def __init__(self):
@@ -13,7 +14,18 @@ class FloatConverter:
         self.shift = -self.min_value * self.scale
 
     def convert(self, value):
-        converted_value = value * self.scale + self.shift
-        if converted_value < self.min_value or converted_value > self.max_value:
-            raise ValueError("Value out of range")
-        return converted_value
+        if isinstance(value, (int, float)):
+            # Single float value
+            converted_value = value * self.scale + self.shift
+            if converted_value < self.min_value or converted_value > self.max_value:
+                raise ValueError("Value out of range")
+            return converted_value
+        elif isinstance(value, np.ndarray):
+            # Array of float values
+            converted_values = value * self.scale + self.shift
+            if np.any(converted_values < self.min_value) or np.any(converted_values > self.max_value):
+                raise ValueError("Value out of range")
+            return converted_values
+        else:
+            raise TypeError("Unsupported input type")
+
--- standalone/TransformLatent_test.py
+++ standalone/TransformLatent_test.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy as np
 from standalone import TransformLatent
 
 class MyTestCase(unittest.TestCase):
@@ -14,6 +15,12 @@ class MyTestCase(unittest.TestCase):
         converted_value = converter.convert(input_native)
         self.assertEqual(0.006493506493506496, converted_value)
 
+    def test_multi(self):
+        input_native = np.array([-1.95, 2.54], dtype=float)
+        converter = TransformLatent.FloatConverter()
+        converted_values = converter.convert(input_native)
+        expected_values = np.array([0.006493506493506496, 0.9783549783549783])
+        np.testing.assert_array_equal(expected_values, converted_values)
 
     def test_OOB(self):
         input_native = -11.95

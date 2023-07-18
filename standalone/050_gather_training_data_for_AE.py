cd90c2f2920d7f329e28d7b84e96685e17806484
Kyle Kreth <100863552+kekreth@users.noreply.github.com>
2023-07-18 19:05:57 -0400

    Might be working now...and rsynced to the server. Now to try and write some "sbatch" jobs that use arrays. Otherwise, I have to submit 1200 * 12 jobs, which I'm NOT sure will go well with the scheduler.

--- standalone/050_gather_training_data_for_AE.py
+++ standalone/050_gather_training_data_for_AE.py
@@ -6,35 +6,37 @@ import time
 import torch
 from CoordinateAnalyzer import CoordinateAnalyzer
 
-# 0) Meta-data file path as GLOBAL variable
-META_DATA_FILE_PATH = "/Users/kkreth/PycharmProjects/cgan/configs/Umass_experiments.txt"
+# Define a global variable for the number of iterations
 ITERATED = 1000
 
-# Read in the meta-data file
-with open(META_DATA_FILE_PATH, 'r') as f:
-    experiment_dict = json.load(f)
 
+def process_file(filename='/Users/kkreth/PycharmProjects/data/DL-PTV/3p6/359.hdf',
+                 META_DATA_FILE_PATH="/Users/kkreth/PycharmProjects/cgan/configs/Umass_experiments.txt"):
+    # Open and load the metadata file as a dictionary
+    with open(META_DATA_FILE_PATH, 'r') as f:
+        experiment_dict = json.load(f)
 
-def process_file(filename='/Users/kkreth/PycharmProjects/data/DL-PTV/3p6/359.hdf'):
+    # Mark the start time to measure the duration of the entire process
     start_time = time.time()
 
-    # 0.5) Delete file if it exists
+    # Generate an output file path, if this file already exists delete it
     output_file_path = filename.replace('.hdf', '_tensors.hdf')
     if os.path.isfile(output_file_path):
         print(f"Deleting existing file: {output_file_path}")
         os.remove(output_file_path)
 
-    # 2) Read file
+    # Read in the hdf file as a DataFrame
     df = pd.read_hdf(filename, key='processed_data')
 
-    # 5) Sort dataframe by x, y, z
+    # Sort the DataFrame by x, y, z
     df.sort_values(by=['x', 'y', 'z'], inplace=True)
 
+    # Initialize an empty list to store the velocity tensors
     velocity_tensors = []
 
-    # 3) Iterations
+    # Iterate over the range defined by ITERATED
     for _ in range(ITERATED):
-        # 6) Trim 2 lowest and 2 highest values from x, y, z arrays
+        # Trim 2 lowest and 2 highest values from x, y, z arrays
         x_enumerated = df['x'].unique()
         y_enumerated = df['y'].unique()
         z_enumerated = df['z'].unique()
@@ -43,26 +45,28 @@ def process_file(filename='/Users/kkreth/PycharmProjects/data/DL-PTV/3p6/359.hdf
         y_enumerated_trimmed = y_enumerated[2:-2]
         z_enumerated_trimmed = z_enumerated[2:-2]
 
-        # ensure trimmed arrays are of expected size
+        # Assert that the trimmed arrays are of expected size
         assert len(x_enumerated_trimmed) == len(x_enumerated) - 4
         assert len(y_enumerated_trimmed) == len(y_enumerated) - 4
         assert len(z_enumerated_trimmed) == len(z_enumerated) - 4
 
-        # 7) Random coordinates
+        # Randomly choose a coordinate from the trimmed arrays
         x_random = random.choice(x_enumerated_trimmed)
         y_random = random.choice(y_enumerated_trimmed)
         z_random = random.choice(z_enumerated_trimmed)
 
-        # 8) Use analyzer to find nearest values
+        # Create a subset DataFrame for analysis
         df_subset = df
 
         try:
-            # Assuming CoordinateAnalyzer is a defined class that takes a DataFrame and provides a method get_nearest_values
+            # Instantiate the CoordinateAnalyzer and get the nearest values to the random coordinate
             analyzer = CoordinateAnalyzer(df_subset)
             result = analyzer.get_nearest_values(x_random, y_random, z_random)
 
+            # Assert that the result contains the expected number of data points
             assert len(result) == 125, "Analyzer does not contain 125 data points"
 
+            # Convert the result to a tensor and append to the velocity_tensors list
             velocity_tensor = torch.tensor(result[['vx', 'vy', 'vz']].values).unsqueeze(0)
 
             velocity_tensors.append(velocity_tensor)
@@ -70,9 +74,10 @@ def process_file(filename='/Users/kkreth/PycharmProjects/data/DL-PTV/3p6/359.hdf
         except Exception as e:
             print(f"Error processing data: {e}")
 
-    # 9) Gather tensors and write them to hdf5
+    # Save the list of velocity tensors as an hdf5 file
     torch.save(velocity_tensors, output_file_path)
 
+    # Print a confirmation that the file has been processed and saved, along with the total runtime
     print(f"File processed and saved in: {output_file_path}")
     print(f"Total run time: {time.time() - start_time} seconds")
 

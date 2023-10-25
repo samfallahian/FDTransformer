0b0cbe8c069790e185c36b521d6b0355bc432329
Kyle Kreth <100863552+kekreth@users.noreply.github.com>
2023-10-25 12:34:42 -0400

    OK, all files are compiled on the server. Now to just write something to iterate through each of those files...running the AE, and storing the result also in that same DF...

--- foundational/AE/CoordinateIterator.py
+++ foundational/AE/CoordinateIterator.py
@@ -1,13 +1,26 @@
 import argparse
-from foundational.AnalyzerOfCoordinates import AnalyzerOfCoordinates
 import pandas as pd
 import numpy as np
 import os
+from foundational.AnalyzerOfCoordinates import AnalyzerOfCoordinates
+
+"""
+For reasons that I don't get, this wouldn't run on the cluters (outside of Pycharm) without this:
+export PYTHONPATH=$PYTHONPATH:/home/kkreth_umassd_edu/cgan/:/home/kkreth_umassd_edu/cgan/foundational
+
+--json "/home/kkreth_umassd_edu/cgan/configs/Umass_experiments.txt" --hdf "/home/kkreth_umassd_edu/DL-PTV/3p6/1.hdf"
+"""
 
 
 class CoordinateIterator:
     DEFAULT_JSON_FILE_LOCATION = "/Users/kkreth/PycharmProjects/cgan/configs/Umass_experiments.txt"
+    #DEFAULT_JSON_FILE_LOCATION = "/home/kkreth_umassd_edu/cgan/configs/Umass_experiments.txt"
     DEFAULT_HDF_FILE = "/Users/kkreth/PycharmProjects/data/DL-PTV/3p6/200.hdf"
+    #DEFAULT_HDF_FILE = "/home/kkreth_umassd_edu/DL-PTV/3p6/200.hdf"
+    """
+    --json /Users/kkreth/PycharmProjects/cgan/configs/Umass_experiments.txt --hdf /Users/kkreth/PycharmProjects/data/DL-PTV/3p6/200.hdf
+    """
+
 
     def __init__(self, json_file_location=None, hdf_file=None):
         self.json_file_location = json_file_location or self.DEFAULT_JSON_FILE_LOCATION
@@ -29,10 +42,18 @@ class CoordinateIterator:
         self.df['centroid_vector'] = self.df.apply(get_coordinates, axis=1)
         self.df.dropna(subset=['centroid_vector'], inplace=True)
 
+        # Transform centroid_vector into 125 separate columns
+        expanded_df = self.df['centroid_vector'].apply(pd.Series)
+        expanded_df.columns = [f'centroid_vector_{str(col).zfill(3)}' for col in expanded_df.columns]
+
+        # Concatenate the new columns to the original DataFrame and drop the centroid_vector column
+        self.df = pd.concat([self.df, expanded_df], axis=1).drop(columns='centroid_vector')
+
         # Saving the pruned DataFrame
         path, filename = os.path.split(self.hdf_file)
-        new_filename = os.path.join(path, f"centroid_coordinates_{filename}")
-        self.df.to_hdf(new_filename, key='df', mode='w', complevel=9, complib='zlib')
+        new_filename = os.path.join(path, f"centroid_coordinates_from_{filename}.pkl.zip")
+        #self.df.to_hdf(new_filename, key='df', mode='w', complevel=9, complib='blosc')
+        self.df.to_pickle(new_filename, compression="zip")
 
     def basic_iterator(self, x, y, z):
         analyzer = AnalyzerOfCoordinates(self.json_file_location, self.hdf_file)
